86c280d1006ca6a89f62f5260abfab38716960a0
Kiril <ki.ned@abv.bg>
2017-03-11 00:31:59 +0000

    fixed logical error in fragment generation and some changes in the layouts of the fragments

--- app/src/main/java/com/sunshineregiment/greenery/Graphs_fragment.java
+++ app/src/main/java/com/sunshineregiment/greenery/Graphs_fragment.java
@@ -14,6 +14,6 @@ public class Graphs_fragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_co2, container, false);
+        return inflater.inflate(R.layout.fragment_graphs, container, false);
     }
 }
--- app/src/main/java/com/sunshineregiment/greenery/Leaderboard_fragment.java
+++ app/src/main/java/com/sunshineregiment/greenery/Leaderboard_fragment.java
@@ -1,19 +1,19 @@
 package com.sunshineregiment.greenery;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * Created by Zikoz on 10/03/2017.
+ * Created by Zikoz on 11/03/2017.
  */
 
-public class Step_counter_fragment extends Fragment {
+public class Leaderboard_fragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_step_counter, container, false);
+        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
     }
 }
